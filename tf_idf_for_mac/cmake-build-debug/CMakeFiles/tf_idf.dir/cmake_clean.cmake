a6471e9c3fed92afff7faf2c664aafd6f0305bdf
EnjoyColin <hnpengjj@gmail.com>
2019-11-12 12:41:43 +0800

    Add files via upload
    tf_idf_for_mac_built_by_CLion

---
+++ tf_idf_for_mac/cmake-build-debug/CMakeFiles/tf_idf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tf_idf.dir/main.cpp.o"
+  "tf_idf"
+  "tf_idf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tf_idf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
